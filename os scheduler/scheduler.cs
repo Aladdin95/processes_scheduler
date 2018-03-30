c2462ada61de41ffd034cdaf261c96aaf7dd0fc3
Abdullah-A-Elsayed <abdullah.ali26@hotmail.com>
2018-03-30 14:36:25 +0200

    RR fixed, all sorting refixed

--- os scheduler/scheduler.cs
+++ os scheduler/scheduler.cs
@@ -27,34 +27,37 @@ namespace os_scheduler
         {
             t.Sort(delegate(Process x, Process y)
             {
-                if (x.arrival == y.arrival) return 1;//swap
-                else if (x.arrival < y.arrival) return 1;//swap
-                else return -1;
+                if (x.arrival > y.arrival) return -1;
+                else return 1;
             });
         }
 
         public void fcfs()
         {
             fcfs_sort(ref input);
-            //input.Reverse();
-            output.Add(new Process(input[input.Count - 1].id, input[input.Count - 1].arrival, input[input.Count - 1].burst,
-                0, input[input.Count - 1].arrival, input[input.Count - 1].arrival + input[input.Count - 1].burst));
 
-            for (int i = input.Count - 2; i > -1; --i)
+            List<Process> temp = new List<Process>(input);
+
+            start = temp.Last().arrival;
+            end = start + temp.Last().burst;
+            output.Add(new Process(temp.Last().id, temp.Last().arrival, temp.Last().burst,
+                temp.Last().priority, start, end));
+            temp.RemoveAt(temp.Count - 1);
+
+            while (temp.Count > 0)
             {
-                if (input[i].arrival < output.Last().end)
+                if (temp.Last().arrival < end)
                 {
-                    output.Add(new Process(input[i].id, input[i].arrival , input[i].burst,
-                        0, output.Last().start + output.Last().burst,
-                         output.Last().start + output.Last().burst + input[i].burst
-                        ));
+                    start = end;
+                    end += temp.Last().burst;
                 }
                 else
                 {
-                    output.Add(new Process(input[i].id, input[i].arrival, input[i].burst,
-                        0, input[i].arrival, input[i].arrival+input[i].burst
-                        ));
+                    start = temp.Last().arrival;
+                    end = start + temp.Last().burst;
                 }
+                output.Add(new Process(temp.Last().id, temp.Last().arrival, temp.Last().burst, temp.Last().priority, start, end));
+                temp.RemoveAt(temp.Count - 1);
             }
         }
 
@@ -79,9 +82,8 @@ namespace os_scheduler
                     {
                         subtemp.Sort(delegate(Process x, Process y)
                         {
-                            if (x.burst == y.burst) return 0;
-                            else if (x.burst < y.burst) return 1;
-                            else return -1;
+                            if (x.burst > y.burst) return -1;
+                            else return 1;
                         });
                         start = end;
                         end += subtemp.Last().burst;
@@ -105,9 +107,8 @@ namespace os_scheduler
 
             subtemp.Sort(delegate(Process x, Process y)
             {
-                if (x.burst == y.burst) return 0;
-                else if (x.burst < y.burst) return 1;
-                else return -1;
+                if (x.burst > y.burst) return -1;
+                else return 1;
             });
 
             while (subtemp.Count() > 0)
@@ -126,12 +127,11 @@ namespace os_scheduler
             {
                 if (x.arrival == y.arrival)
                 {
-                    if (x.burst == y.burst) return 0;
-                    else if (x.burst < y.burst) return 1;
-                    else return -1;
+                    if (x.burst > y.burst) return -1;
+                    else return 1;
                 }
-                else if (x.arrival < y.arrival) return 1;
-                else return -1;
+                else if (x.arrival > y.arrival) return -1;
+                else return 1;
             }
             );
         }
@@ -143,12 +143,11 @@ namespace os_scheduler
             {
                 if (x.arrival == y.arrival)
                 {
-                    if (x.priority == y.priority) return 0;
-                    else if (x.priority < y.priority) return 1;
-                    else return -1;
+                    if (x.priority > y.priority) return -1;
+                    else return 1;
                 }
-                else if (x.arrival < y.arrival) return 1;
-                else return -1;
+                else if (x.arrival > y.arrival) return -1;
+                else return 1;
             }
             );
         }
@@ -335,9 +334,8 @@ namespace os_scheduler
                     {
                         subtemp.Sort(delegate(Process x, Process y)
                         {
-                            if (x.priority == y.priority) return 0;
-                            else if (x.priority < y.priority) return 1;
-                            else return -1;
+                            if (x.priority > y.priority) return -1;
+                            else return 1;
                         });
                         start = end;
                         end += subtemp.Last().burst;
@@ -361,9 +359,8 @@ namespace os_scheduler
 
             subtemp.Sort(delegate(Process x, Process y)
             {
-                if (x.priority == y.priority) return 0;
-                else if (x.priority < y.priority) return 1;
-                else return -1;
+                if (x.priority > y.priority) return -1;
+                else return 1;
             });
 
             while (subtemp.Count() > 0)
@@ -375,7 +372,7 @@ namespace os_scheduler
             }
         }
 
-        static int min(int x, int y) { return (x > y) ? y : x; }
+        public int min(int x, int y) { return (x > y) ? y : x; }
 
         public void RR(int quantum)
         {
@@ -391,63 +388,35 @@ namespace os_scheduler
             fcfs_sort(ref temp);
 
             start = temp.Last().arrival;
-            end = 0;
-            while (temp.Count > 0 && temp.Last().burst < quantum)
-            {
-                end = temp.Last().arrival + temp.Last().burst;
-                output.Add(new Process(temp.Last().id, temp.Last().arrival, temp.Last().burst, temp.Last().priority, start, end));
-                temp.RemoveAt(temp.Count - 1);
-                start = end;
-            }
-
-            if (temp.Count > 0)
-            {
-                end += quantum;
-                output.Add(new Process(temp.Last().id, temp.Last().arrival, temp.Last().burst, temp.Last().priority, start, end));
-                if (temp.Last().burst > quantum)
-                {
-                    output.Last().burst = quantum;
-                    temp.Last().burst -= quantum;
-                    temp.Last().arrival = end;
-                    fcfs_sort(ref temp);
-                }
-                else
-                    temp.RemoveAt(temp.Count - 1);
-            }
+            end = start + temp.Last().burst;
+            output.Add(new Process(temp.Last().id, temp.Last().arrival, temp.Last().burst, temp.Last().priority, start, end));
+            temp.RemoveAt(temp.Count - 1);
 
             while (temp.Count > 0)
             {
-                if (temp.Last().arrival > output.Last().end)
+                if (temp.Last().arrival > (output.Last().start + min(quantum, output.Last().burst)))
                 {
-                    if (front != rear)
+                    if (front == rear)
                     {
-                        start = end;
-                        end += min(subtemp[front].burst, quantum);
-                        output.Add(new Process(subtemp[front].id, subtemp[front].arrival, subtemp[front].burst, subtemp[front].priority, start, end));
-                        if (subtemp[front].burst > quantum)
-                        {
-                            output.Last().burst = quantum;
-                            subtemp[front].burst -= quantum;
-                            subtemp[front].arrival = end;
-                            temp.Add(subtemp[front]);
-                            fcfs_sort(ref temp);
-                        }
-                        front = (front + 1) % (nprocess + 1);
+                        start = temp.Last().arrival;
+                        end = temp.Last().arrival + temp.Last().burst;
+                        output.Add(new Process(temp.Last().id, temp.Last().arrival, temp.Last().burst, temp.Last().priority, start, end));
+                        temp.RemoveAt(temp.Count - 1);
                     }
                     else
                     {
-                        start = temp.Last().arrival;
-                        end = temp.Last().arrival + min(temp.Last().burst, quantum);
-                        output.Add(new Process(temp.Last().id, temp.Last().arrival, temp.Last().burst, temp.Last().priority, start, end));
-                        if (temp[front].burst > quantum)
+                        if (output.Last().burst > quantum)
                         {
+                            subtemp[rear] = new Process(output.Last());
+                            subtemp[rear].burst -= quantum;
+                            rear = (rear + 1) % (nprocess + 1);
                             output.Last().burst = quantum;
-                            temp.Last().burst -= quantum;
-                            temp.Last().arrival = end;
-                            fcfs_sort(ref temp);
+                            output.Last().end = output.Last().start + quantum;
                         }
-                        else
-                            temp.RemoveAt(temp.Count - 1);
+                        start = output.Last().end;
+                        end = start + subtemp[front].burst;
+                        output.Add(new Process(subtemp[front].id, subtemp[front].arrival, subtemp[front].burst, subtemp[front].priority, start, end));
+                        front = (front + 1) % (nprocess + 1);
                     }
                 }
                 else
@@ -460,16 +429,17 @@ namespace os_scheduler
 
             while (rear != front)
             {
-                start = end;
-                end += min(subtemp[front].burst, quantum);
-                output.Add(new Process(subtemp[front].id, subtemp[front].arrival, subtemp[front].burst, subtemp[front].priority, start, end));
-                if (subtemp[front].burst > quantum)
+                if (output.Last().burst > quantum)
                 {
+                    subtemp[rear] = new Process(output.Last());
+                    subtemp[rear].burst -= quantum;
                     output.Last().burst = quantum;
-                    subtemp[front].burst -= quantum;
-                    subtemp[rear] = (subtemp[front]);
+                    output.Last().end = output.Last().start + quantum;
                     rear = (rear + 1) % (nprocess + 1);
                 }
+                start = output.Last().end;
+                end = start + subtemp[front].burst;
+                output.Add(new Process(subtemp[front].id, subtemp[front].arrival, subtemp[front].burst, subtemp[front].priority, start, end));
                 front = (front + 1) % (nprocess + 1);
             }
         }
